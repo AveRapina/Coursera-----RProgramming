f6b9990ea803fd6b56fc6d598b3813af6a5e134b
HSO <hugo.soares@fe.up.pt>
2016-01-12 23:15:43 +0000

    Add Quiz3 Comments

--- Peer/Week3/cachematrix.R
+++ Peer/Week3/cachematrix.R
@@ -2,7 +2,12 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+The first function, makeVector creates a special "matrix", 
+#which is really a list containing functions to
+# set the value of the matrix
+# get the value of the matrix  
+# set the value of the inverse
+# get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <-function(y){
@@ -19,6 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+#The following function calculates the inverse of the special "Matrix" created with the above function.
+#However, it first checks to see if the inverse has already been calculated. 
+#If so, it gets the inverse from the cache and skips the computation. 
+#Otherwise, it calculates the inverse of the data and sets the value of the
+#inverse in the cache via the setInv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -30,4 +40,35 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         inv <-solve(data,...)
         x$setInv(inv)
+        inv # return
 }
+
+
+
+# makeVector <- function(x = numeric()) {
+#         m <- NULL
+#         set <- function(y) {
+#                 x <<- y
+#                 m <<- NULL
+#         }
+#         get <- function() x
+#         setmean <- function(mean) m <<- mean
+#         getmean <- function() m
+#         list(set = set, get = get,
+#              setmean = setmean,
+#              getmean = getmean)
+# }
+# 
+# 
+# 
+# cachemean <- function(x, ...) {
+#         m <- x$getmean()
+#         if(!is.null(m)) {
+#                 message("getting cached data")
+#                 return(m)
+#         }
+#         data <- x$get()
+#         m <- mean(data, ...)
+#         x$setmean(m)
+#         m
+# }
